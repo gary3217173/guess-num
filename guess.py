b811d1ad4fd18e71cbe8efad16a8a4577c5e1131
Gary Chen <gary3217173@gmail.com>
2019-04-22 21:21:09 +0800

    basic version

---
+++ guess.py
@@ -0,0 +1,12 @@
+import random
+r = random.randint(1, 100)
+while True:
+	num = input('請猜一個隨機整數: ')
+	num = int(num)
+	if num == r:
+		print('恭喜你猜對了')
+		break
+	elif num > r:
+		print('比答案大')
+	elif num < r: 
+		print('比答案小')
